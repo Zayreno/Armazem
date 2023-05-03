1b7caae46f16a056d9bcc4f36af4b07db126d964
João Morais <joaopmorais95@gmail.com>
2023-05-03 16:06:10 +0100

    Adição da função para adicionar artigos e correção da função para desenhar o armazém no que diz respeito ao range.

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def listarArtigos():
         x.print()
 
 def desenhaArmazem():
-    for x in range(0, 10):
-        for y in range(0, 10):
+    for x in range(1, 11):
+        for y in range(1, 11):
             quantidade = quantidadeItems(x, y)
             print(str(quantidade) + "\t", end="")
 
@@ -32,10 +32,30 @@ def editaQuantidade():
             q.quantidade = int(input("Indique quantas peças lá estão:\n"))
             break
 
+def adicionarPecas():
+    nomePeca = input("Indique o nome da peça:\n")
+    quantidadePeca = int(input("Indique quantas peças lá estão:\n"))
+    x, y = input("Indique onde quer guardar as peças:(X Y)\n").split(" ")
+    x = int(x)
+    y = int(y)
+
+    meuLocal = Local(x, y)
+
+    for i in listaPecas:
+        if i.nome == nomePeca:
+            i.quantidade = i.quantidade + quantidadePeca
+            print("Já lá tinha peças, o total agora é de " + str(i.quantidade) + " peças.")
+        elif i.local == meuLocal:
+            print("Já se encontram outras peças aí.")
+
+    novaPeca = Peca(nomePeca, quantidadePeca, meuLocal)
+    listaPecas.append(novaPeca)
+
 
 escolha = -1
 while escolha != 0:
-    escolha = int(input("\n0. Sair\n1. Listar artigos\n2. Desenhar armazém\n3. Editar quantidades\n"))
+    escolha = int(input("\n0. Sair\n1. Listar artigos\n2. Desenhar armazém\n"
+                        "3. Editar quantidades\n4. Adicionar peças\n"))
     match escolha:
         case 0:
             break
@@ -45,6 +65,8 @@ while escolha != 0:
             desenhaArmazem()
         case 3:
             editaQuantidade()
+        case 4:
+            adicionarPecas()
         case _:
             print("Escolha inválida")
 
